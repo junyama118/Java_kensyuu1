cd2278ff83ad3e75e175f8d223fb07d83d15c4e1
Jum Yamahira <jun.yamahira@gmo-media.jp>
2020-04-22 14:59:56 +0900

    [add] 012~015

---
+++ src/Cls_015.java
@@ -0,0 +1,10 @@
+public class Cls_015 {
+    String str;
+
+    Cls_015() {
+        str = "no name";
+    }
+    Cls_015(final String str) {
+        this.str = str;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        AAA cls = new AAA();
+        //AAA cls = new AAA();
+        System.out.println(new Cls_015("aaa").str);
+        System.out.println(new Cls_015().str);
     }
 }
